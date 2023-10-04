6d887772eb3a278fe174c5f547048100b13b4f7a
Matt Kramer <mjkramer@users.noreply.github.com>
2023-10-03 18:55:35 -0700

    rm copy_inputs.sh

--- copy_inputs.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-mkdir inputs
-cp /global/cfs/cdirs/dune/www/data/2x2/simulation/larndsim_data/light_LUT_M123_v1/lightLUT_M123.npz inputs
-cp /global/cfs/cdirs/dune/users/mkramer/mywork/2x2_sim/run-convert2h5/output/MiniRun4_1E19_RHC.convert2h5/EDEPSIM_H5/MiniRun4_1E19_RHC.convert2h5.00246.EDEPSIM.h5 inputs
