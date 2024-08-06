987e0c98c9e68916c95be7ae862e80b96a8ccf01
Matt Kramer <mjkramer@users.noreply.github.com>
2024-08-05 22:46:42 -0700

    Add run.sh and larnd-sim.sh

---
+++ larnd-sim.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+default_in_file="/global/cfs/cdirs/dune/www/data/2x2/simulation/mkramer_dev/larnd-sim-example/MiniRun5_1E19_RHC.convert2h5.00123.EDEPSIM.hdf5"
+default_config="2x2"
+
+# allow custom input file to be passed via command line
+in_file=${LARNDSIM_INPUT_FILE:-$default_in_file}
+config=${LARNDSIM_CONFIG:-$default_config}
+
+extra_args=()
+
+if [[ -n "$LARNDSIM_MAX_EVENTS" ]]; then
+    extra_args+=("--n_events" "$LARNDSIM_MAX_EVENTS")
+fi
+
+cd "$(dirname "${BASH_SOURCE[0]}")"
+source larnd-sim.venv/bin/activate
+
+now=$(date -u +%Y%m%dT%H%M%SZ)
+
+out_file=$(basename "$in_file" .hdf5 | sed 's/convert2h5/larnd/' | sed 's/.EDEPSIM//')."$now".LARNDSIM.hdf5
+out_dir=$SCRATCH/larnd-sim-output
+mkdir -p "$out_dir"
+
+simulate_pixels.py "$config" \
+    --input_filename "$in_file" \
+    --output_filename "$out_dir/$out_file" \
+    --rand_seed 321 "${extra_args[@]}"
---
+++ run.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+module unload python 2>/dev/null
+module unload cudatoolkit 2>/dev/null
+
+module load cudatoolkit/12.4
+module load python/3.11
+
+cd $(dirname "${BASH_SOURCE[0]}")
+source larnd-sim.venv/bin/activate
+
+nsys=/global/common/software/dune/mkramer/misc_software/nsight-systems-2023.4.1/bin/nsys
+ncu=/global/common/software/dune/mkramer/misc_software/NVIDIA-Nsight-Compute-2024.1/ncu
+
+run_in_nsys() {
+    mkdir -p $out_dir/nsys
+    # --cudabacktrace=all gives us a crash
+    # nsys profile --cuda-memory-usage=true --cudabacktrace=all --python-backtrace=cuda --python-sampling=true \
+    $nsys profile --cuda-memory-usage=true --python-backtrace=cuda --python-sampling=true \
+        -o $out_dir/nsys/nsys-report%n "$@"
+}
+
+run_in_ncu() {
+    mkdir -p $out_dir/ncu
+    dcgmi profile --pause
+    $ncu --kernel-id :::5 -o $out_dir/ncu/ncu-report.$now "$@"
+    # $ncu -o $out_dir/ncu/ncu-report.$now "$@"
+    dcgmi profile --resume
+}
+
+if [[ "$LARNDSIM_PROFILER" == "nsys" ]]; then
+    run_in_nsys "$@"
+elif [[ "$LARNDSIM_PROFILER" == "ncu" ]]; then
+    run_in_ncu "$@"
+else
+    "$@"
+fi
