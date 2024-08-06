48b293792eb9ee34237c3e1e5022280895ce3f31
Matt Kramer <mjkramer@users.noreply.github.com>
2024-08-05 22:43:15 -0700

    Pin cupy to 13.1.0 to avoid issue with numpy 2.0
    https://github.com/cupy/cupy/issues/8392

--- install_larnd_sim.sh
+++ install_larnd_sim.sh
@@ -10,9 +10,9 @@ python3 -m venv larnd-sim.venv
 source larnd-sim.venv/bin/activate
 pip install --upgrade pip setuptools wheel
 # For validation plots:
-pip install matplotlib awkward
+# pip install matplotlib awkward
 
 git clone -b develop https://github.com/DUNE/larnd-sim
 cd larnd-sim
-pip install cupy-cuda11x
+pip install cupy-cuda12x==13.1.0
 SKIP_CUPY_INSTALL=1 pip install -e .
