3df0ee3a62d3d2d1435e06223fa2ea572a3d3d82
Madan Timalsina <49295484+madantimalsina@users.noreply.github.com>
2025-02-26 10:35:44 -0800

    Update install_larnd_sim.sh
    update an example after forked

--- install_larnd_sim.sh
+++ install_larnd_sim.sh
@@ -11,10 +11,10 @@ pip install matplotlib awkward
 
 # If we're re-running the installer for, say, a different CUDA version, then
 # `larnd-sim` already exists
-if [[ ! -d larnd-sim ]]; then
+if [[ ! -d larnd-sim-hackathon2024 ]]; then
     # git clone -b develop https://github.com/DUNE/larnd-sim
     git clone -b develop https://github.com/madantimalsina/larnd-sim-hackathon2024
 fi
 
-cd larnd-sim
+cd larnd-sim-hackathon2024
 pip install -e .
